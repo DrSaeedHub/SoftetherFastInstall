74d3500e3e1edc896e0eee8e4e1c5987f0c96b02
DrSaeedHub <122322581+DrSaeedHub@users.noreply.github.com>
2024-09-22 03:31:52 +0330

    se-install.sh

--- se-install.sh
+++ se-install.sh
@@ -51,9 +51,9 @@ apt-get install -y wget net-tools build-essential checkinstall dos2unix libssl-d
 mkdir -p /tmp/softether-autoinstall
 cd /tmp/softether-autoinstall
 
-# Download SoftEther | Version 4.38 | Build 9760
+# Download SoftEther | Version 4.43 | Build 9799
 printf "\nDownloading release: ${RED}4.38 RTM${NC} | Build ${RED}9760${NC}\n\n"
-wget -O vpnserver.tar.gz https://www.softether-download.com/files/softether/v4.38-9760-rtm-2021.08.17-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
+wget -O vpnserver.tar.gz http://www.softether-download.com/files/softether/v4.43-9799-beta-2023.08.31-tree/Linux/SoftEther_VPN_Client/64bit_-_Intel_x64_or_AMD64/softether-vpnclient-v4.43-9799-beta-2023.08.31-linux-x64-64bit.tar.gz
 
 # Extract the archive
 tar -xzf vpnserver.tar.gz
